271ba9b51ab4e88624dfe599bb5564887a7fe251
geo-liak <liakopoulos.georgios@gmail.com>
2019-05-09 02:13:24 +0300

    Update GeneralController.java

--- src/main/java/com/pizzahouse/controller/GeneralController.java
+++ src/main/java/com/pizzahouse/controller/GeneralController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class UserController {
+public class GeneralController {
 
     @Autowired
     private UserService userService;
@@ -31,46 +31,6 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
-
-    @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String registration(Model model) {
-        model.addAttribute("userForm", new User());
-
-        return "pages/registration";
-    }
-
-    @RequestMapping(value = "/contact", method = RequestMethod.GET)
-    public String contact(Model model) {
-        return "pages/contact";
-    }
-
-    @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
-        userValidator.validate(userForm, bindingResult);
-
-        if (bindingResult.hasErrors()) {
-            return "/pages/registration";
-        }
-
-        userService.save(userForm);
-
-        securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
-
-        return "redirect:/";
-    }
-
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
-        if (error != null) {
-            model.addAttribute("error", "Your username and password is invalid.");
-        }
-
-        if (logout != null) {
-            model.addAttribute("message", "You have been logged out successfully.");
-        }
-
-        return "/pages/login";
-    }
 
     @RequestMapping(value = {"/", "home"}, method = RequestMethod.GET)
     public String home(HttpServletResponse response, Principal principal, Model model, String error, String logout) {
@@ -101,9 +61,45 @@ public class UserController {
         return "/home";
     }
 
-    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
-    public String welcome(Model model
-    ) {
-        return "/pages/welcome";
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "pages/registration";
     }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "/pages/registration";
+        }
+
+        userService.save(userForm);
+
+        securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
+
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String contact(Model model) {
+        return "pages/contact";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null) {
+            model.addAttribute("error", "Your username and password is invalid.");
+        }
+
+        if (logout != null) {
+            model.addAttribute("message", "You have been logged out successfully.");
+        }
+
+        return "/pages/login";
+    }
+
+  
 }
